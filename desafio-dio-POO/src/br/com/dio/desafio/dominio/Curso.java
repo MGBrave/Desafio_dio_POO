92688f18cd7c06ee4ec308738819449cbec87ff3
Marta Geraldo <60014891+martageraldo@users.noreply.github.com>
2022-01-25 15:25:06 -0300

    Initial

---
+++ desafio-dio-POO/src/br/com/dio/desafio/dominio/Curso.java
@@ -0,0 +1,49 @@
+package br.com.dio.desafio.dominio;
+
+public class Curso extends Conteudo {
+    private String Titulo; //atributos
+    private String descricao; //atributos
+    private int cargaHoraria; //atributos
+
+    public Curso() {
+    }
+
+    @Override
+    public double calcularXp() {
+        return 0;
+    }
+
+    public String getTitulo() {
+        return Titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        Titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    @Override
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "Titulo='" + Titulo + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", cargaHoraria=" + cargaHoraria +
+                '}';
+    }
+}
